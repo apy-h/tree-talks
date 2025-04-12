2730e8e8c21179a3f8a6661e1be5bfb674160482
apy-h <141757591+apy-h@users.noreply.github.com>
2025-04-12 22:05:27 +0000

    hacked land plants together :) :(

---
+++ landPlantData.js
@@ -0,0 +1,72 @@
+export const treeData = {
+    name: "Land Plants (Embryophytes)",
+    description: "",
+    traits: [],
+    children: [
+      {
+        name: "Bryophytes",
+        description: "",
+        traits: [],
+        children: [
+          {
+            name: "Hornworts",
+            description: "",
+            traits: [],
+            children: []
+          },
+          {
+            name: "Liverworts",
+            description: "",
+            traits: [],
+            children: []
+          },
+          {
+            name: "Mosses",
+            description: "",
+            traits: [],
+            children: []
+          }
+        ]
+      },
+      {
+        name: "Vascular Plants",
+        description: "",
+        traits: [],
+        children: [
+          {
+            name: "Lycophytes",
+            description: "",
+            traits: [],
+            children: [
+              {
+                name: "Lycopodium",
+                description: "",
+                traits: [],
+                children: []
+              },
+              {
+                name: "Selaginella",
+                description: "",
+                traits: [],
+                children: [
+                  {
+                    name: "Isoetes",
+                    description: "",
+                    traits: [],
+                    children: []
+                  },
+                  {
+                    name: "Tree-sized Lycophytes",
+                    description: "Extinct",
+                    traits: [],
+                    children: []
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    ],
+    polyphyleticGroups: []
+  };
